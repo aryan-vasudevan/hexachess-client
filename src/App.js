c61f8708744a966daac5bb8eb33828f3bc8a2099
Aryan <vasudevan.aryan@gmail.com>
2024-08-20 21:27:08 -0400

    Add tile.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import tile from "./img/tile.png";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1 className="text-center text-[2em]">hexachess.io</h1>
+            <img src={tile} alt="tile" height={40} width={40} />
+        </div>
+    );
 }
 
 export default App;
